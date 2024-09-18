209696cfa1450f7907d885fb39a1f50e2ec93ede
Julian Cutaya <ctj5223a@ensfsi.onmicrosoft.com>
2024-09-18 15:28:09 +0200

    15:27_TP1$

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -25,12 +25,17 @@ public class Gaulois {
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
 				+ romain.getNom());
-		romain.recevoirCoup(force/3);
+		romain.recevoirCoup((force/3)*effetPotion);
 	}
 
 	@Override
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public void boirePotion(int forcePotion) {
+		this.effetPotion += forcePotion;
+		parler("Merci Druide, je sens que ma force est " + forcePotion + " fois décuplée. ")
 	}
 	
 	public static void main(String[] args) {
